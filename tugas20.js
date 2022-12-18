87508a307d50c1afacac930d27f85b5b1f44e690
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-18 09:25:20 +0700

    Mengerjakan Tugas 20 Javascript

---
+++ tugas20.js
@@ -0,0 +1,17 @@
+// let angka = 3452;
+
+// console.log(angka);
+// console.log(angka.toString());
+
+// let kalimat = "Mesak Angga";
+
+// console.log(kalimat.indexOf("Angga", 8));
+// console.log(kalimat.lastIndexOf("Angga"));
+// console.log(kalimat.search("e"));
+// console.log(kalimat.search(/Angga/));
+// console.log(kalimat.search(/Angga/i));
+// console.log(kalimat.match("Angga"));
+
+let kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable";
+
+console.log(kalimat.match("are"), kalimat.match("words"));
